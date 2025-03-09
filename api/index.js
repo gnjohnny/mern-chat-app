f79b065c020e022c14b19695ecd795dc2c7cb8d9
John <54johnmbugua@gmail.com>
2025-03-10 00:53:56 +0300

    updated axios base url

--- api/index.js
+++ api/index.js
@@ -51,7 +51,7 @@ app.use("/api/messages", checkAuth, MessagesRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client/dist", "index.html"));
     });
 }
 
--- client/src/lib/axios.lib.js
+++ client/src/lib/axios.lib.js
@@ -3,4 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: "https://chatty-5vu9.onrender.com/api",
     withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
